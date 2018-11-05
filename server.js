4b804d399049dc1d2e0aaed03977c4fb3d2f7cb1
luis juarez <luis.juarez2394@gmail.com>
2018-11-04 19:59:03 -0600

    cambios finales en el dockerfile

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //declaracion de cliente mongo db y ConnectionString
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://mongodb:27017/";
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var redis = require("redis");
-var redis_client = redis.createClient();
+var redis_client = redis.createClient(process.env.REDIS_URL);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
